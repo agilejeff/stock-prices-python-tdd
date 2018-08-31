4003418aa1567a2a29db14123d5e85bc4727ff1f
AgileJeff <jeffjohnsonpm@gmail.com>
2018-08-31 10:43:47 -0400

    Failing Test - equityStock function returns value passed

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -6,6 +6,11 @@ class TestGetPrice(unittest.TestCase):
     def test_assert_equitySymbol_requires_nonblankstring(self):
         self.assertIsNotNone(stocks.equitySymbol())
 
+    def test_equitySymbol_uses_provided_code(self):
+        self.assertEqual("EON_X", stocks.equitySymbol("EON_X"))
+
+        
+
 
 
 
