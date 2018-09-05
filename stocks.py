b3344b1cde837588fb4314629ba85906b3da0f6d
AgileJeff <jeffjohnsonpm@gmail.com>
2018-09-05 15:43:41 -0400

    Failed Test - price function exists

--- stocks.py
+++ stocks.py
@@ -1,7 +1,18 @@
 # python file for code
 
+
+
 # Determine stock symbol
+
 def equitySymbol(targetSymbol="WAC_X"):
     equityLookupSymbol = targetSymbol
     return equityLookupSymbol
 
+
+
+
+
+
+
+
+# https://www.quandl.com/api/v3/datasets/FSE/EON_X.json?api_key=qxkJ2yNa3k5tUpnZPzAh&start_date=1970-01-01&end_date=1970-01-01
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -6,10 +6,15 @@ class TestGetPrice(unittest.TestCase):
     def test_assert_equitySymbol_requires_nonblankstring(self):
         self.assertIsNotNone(stocks.equitySymbol())
 
+
+
     def test_equitySymbol_uses_provided_code(self):
         self.assertEqual('EON_X', stocks.equitySymbol("EON_X"))
+        self.assertEqual('WAC_X', stocks.equitySymbol())
 
-
+    def test_price_is_number_greater0(self):
+        price = stocks.priceLookup("EON_X")
+        self.assertGreater(price, 0)
 
 
 
