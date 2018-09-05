b8ef1941dfaf10822154583ebe613f2047c00589
AgileJeff <jeffjohnsonpm@gmail.com>
2018-09-05 17:23:09 -0400

    Failed Test - function to launch webui should exist

--- tests/tests_stockswebui.py
+++ tests/tests_stockswebui.py
@@ -1,1 +1,7 @@
 import stockswebui
+import unittest
+
+class StocksWebUITests(unittest.TestCase):
+    def test_webpage_module_launches_no_error(self):
+        result = stockswebui.launchWebsite()
+        self.assertTrue(result)
