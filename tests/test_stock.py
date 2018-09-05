3379d4fade12e2183ad83b72eddff188b243d017
AgileJeff <jeffjohnsonpm@gmail.com>
2018-09-05 16:57:46 -0400

    Failed Test - module for webui should exist

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -6,8 +6,6 @@ class TestGetPrice(unittest.TestCase):
     def test_assert_equitySymbol_requires_nonblankstring(self):
         self.assertIsNotNone(stocks.equitySymbol())
 
-
-
     def test_equitySymbol_uses_provided_code(self):
         self.assertEqual('EON_X', stocks.equitySymbol("EON_X"))
         self.assertEqual('WAC_X', stocks.equitySymbol())
