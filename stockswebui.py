c952edd1cc38c4aa5020429d476693e0d6639384
AgileJeff <jeffjohnsonpm@gmail.com>
2018-09-05 21:08:47 -0400

    Passed Test - function to launch webui should exist - code works to get stock price, but testing framework does not test

--- stockswebui.py
+++ stockswebui.py
@@ -1,4 +1,27 @@
 #web ui stuff
 
+import cherrypy
+import stocks
+
+class GetStockSymbol(object):
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+            <head></head>
+            <body>
+                <form method="get" action="price">
+                    <input type="text" value="EON_X" name="symbol" />
+                    <button type="submit">Lookup Price</button>
+                </form>
+            </body>
+        </html>"""
+
+    @cherrypy.expose
+    def price(self, symbol="WAC_X"):
+        data_price = stocks.getPriceWeb(symbol)
+        return "Closing Price for " + symbol + " on Sep 3 was " + str(data_price)
+
+
 def launchWebsite():
+    cherrypy.quickstart(GetStockSymbol())
     return True
